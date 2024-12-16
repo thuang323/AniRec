f567dc125db114f60cc70f97d973de759db5bce3
jyun2003 <osuhuang.4978@gmail.com>
2024-12-16 02:14:07 -0500

    added layout for login page

---
+++ firebase/index.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCXeLNyeLygJxUWLaHDhGZgeK4qKHxU4SM",
+  authDomain: "anirec-902e9.firebaseapp.com",
+  projectId: "anirec-902e9",
+  storageBucket: "anirec-902e9.firebasestorage.app",
+  messagingSenderId: "517020752686",
+  appId: "1:517020752686:web:33ee2b2de08713d888b8bf",
+  measurementId: "G-W1F98SSF39",
+};
+
+const app = initializeApp(firebaseConfig);
